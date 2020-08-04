fb91bee87bdb31d9ed9fcfa4ad5c404f624d1c6f
Georgios Mpovolis <gam154@scarletmail.rutgers.edu>
2020-08-04 09:41:28 -0400

    change the logic for assigning present past and future css elements and updated local storage logic

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Work-Day-Scheduler
+
+This application is a simple work day scheduler that allows the user to visually see a hour blocks for a 9-5 time schedule.
+
+In a jumbotron there is the name of the application, along with a brief description and the current day, month, and day of the month.
+
+There are three fields in each row. A time block, and text block and a save block.
+
+The time block displays which hour in the schedule alond with an AM or PM value.
+
+The text blocks are color coded to seperate between past(gray), present(red), and future(green). The user can also type text into those blocks to add tasks or whatever the user wishes.
+
+The save block is a button that when clicked will save the text in the text block to the local storage. 
+
+When the schedule is build by the for loop it will add the local storage values to the value of the corresponding input text row.
+
+Note* for local storage usage the value associated with each key that corresponds to each input text field will be cleared to "" if the input field is blank when saved
--- javascript.js
+++ javascript.js
@@ -1,28 +1,93 @@
-$("#currentDay").text(moment().format("dddd MMMM Do"));
+$(function initialize(){
+    $("#currentDay").text(moment().format("dddd, MMMM Do"));
+    //puts the day month number of day in the month in the jumbotron
 
 var time = moment().format("LT");
-console.log(time);
+console.log("time: " + time);
 var currentHour = moment().format("LT").charAt(0);
-console.log("Current Hour: " + currentHour);
+if(moment().format('LT').charAt(1)==1 || moment().format('LT').charAt(1)==2){
+    currentHour += moment().format('LT').charAt(1);
+}
+//pulls the current hour from the moment.js
+console.log("currentHour: " + currentHour);
+
 var currentAm = moment().format("LT").charAt(5);
 currentAm += moment().format("LT").charAt(6);
 console.log("AM or PM: " + currentAm);
-var hour = currentHour + " " + currentAm;
-var startHour = moment().subtract(4, 'hour').format('LT').charAt(0);
-console.log("Start hour: " + startHour);
-var index = 0;
+//pulls if it is AM or PM
 
-for(var i = 0; i<10; i++){
-    $("<div>")
-    var row = $("myRow").append('<div class="row past future present saveBtn time-block description"  id="myRow"+index></div>'); 
-    $(".container").append(row);
-    if(parseInt(startHour)>parseInt(currentHour)){
-        $("myRow")
-        }; 
-    if(startHour===currentHour){
-    $("myRow")
-    };
+var index = 0;
+//index increments with each row
+
+var localStartHour = 9;
+//initializes the local start hour to 9
+//this variable is for comparisons with the value of the milHour
+
+var timeDisplay = 9;
+//initializes the time for the time display
+var localAmPm = "AM";
+// AM or PM variable for the time Display 
+
+var milHour = currentHour;
+if(currentAm==="PM"){
+    milHour= milHour + 12;
+}
+console.log("milHour: " + milHour)
+// initializes a military time variable for easy comparisons and sets it to the current hour also changes it into military time if it is PM
+
+for(var i = 0; i<9; i++){
+    $(".container").append('<div class="row"  id="myRow'+index+'"></div>');
+    //creates the row that the three elements being created go in
+
+    var hourDisplay = $("<div class='hour time-block' id='hour" +index+ "'>");
+    $("#myRow"+index).append(hourDisplay);
+    //creates the hour display section in the row
+    $("#hour"+index).text(timeDisplay +  " " + localAmPm);
+    var pastInput = $("<input type='text' class='past description' id='input"+index+"'>")
+    var presentInput = $("<input type='text' class='present description' id='input"+index+"'>")
+    var futureInput = $("<input type='text' class='future description' id='input"+index+"'>");
+
+    if(milHour<localStartHour){
+        //set to future
+        $("#myRow"+index).append(futureInput);
+    } else if(milHour==localStartHour){
+        // set to present
+        $("#myRow"+index).append(presentInput);
+    } else{
+        // set to past
+        $("#myRow"+index).append(pastInput);        
+    }
+    if(!localStorage.getItem('text'+index=="")){
+    $("#input"+index).text(localStorage.getItem('text'+index))
     
-    $("div").append(hour + " " + currentAm);
+    //adds local storage info for that indexed row
+    }
+   
+    var makeBtn = $('<button class="saveBtn" id="btn'+index+'">');
+    $("#myRow"+index).append(makeBtn);
+    $("button").text("Save");
+    // make save button
+    
+    console.log('milHour: '+ milHour+ ' hourDisplay: '+ timeDisplay + ' localStartHour: ' +  localStartHour + ' Index: ' + index);
+    localStartHour++;
+    timeDisplay++;
+    index++;
+    if(timeDisplay===13){
+        timeDisplay = 1;
+        localAmPm = "PM"
+        //Sets the localAmPm variable to PM and sets timeDisplay to begin the PM values display
+    }
 }
 
+$("button").on("click", function(event){
+    event.preventDefault();
+    console.log(this.id);
+    var clickIndex = this.id.charAt(3);
+    //sets clickIndex to the row index of the button clicked
+    var inputText = $("#input"+clickIndex).val();
+    // sets inputText to the text in the input field in the row of the button clicked
+    localStorage.setItem('text'+clickIndex, inputText);
+    // saves the inputText to a text+clickIndex key related to the row clicked
+console.log(localStorage.getItem('text'+clickIndex));
+});
+})
